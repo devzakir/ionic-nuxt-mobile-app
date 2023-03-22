775697fef340709fe424034043cb1a21181b3e4e
Zakir Hossen <web.zakirbd@gmail.com>
2023-03-22 13:14:01 +0600

    build: typescript support added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,9 @@ module.exports = {
   },
   parser: "vue-eslint-parser",
   parserOptions: {
-    //   parser: '@typescript-eslint/parser',
+    parser: "@typescript-eslint/parser",
   },
-  extends: ["plugin:prettier/recommended"],
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
   plugins: [],
   rules: {},
 };
